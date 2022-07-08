da96098183e07d1661422a91f9b41e4828a77eb1
QianYu <597667983@qq.com>
2022-07-08 11:15:34 +0800

    Fist commit

---
+++ JavaBasis/src/main/java/com/qy/datatypesandoperators/DataType.java
@@ -0,0 +1,16 @@
+package com.qy.datatypesandoperators;
+
+import org.junit.Test;
+
+/**
+ * @Author QianSiWang
+ * @Date 2022/7/8 11:12
+ * @Description Java的数据类型
+ */
+public class DataType {
+
+    @Test
+    public void intTest(){
+
+    }
+}
