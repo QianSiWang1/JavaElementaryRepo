cb1999c90427e8ca9775f86c0dd87a4f450bd8bd
QianYu <597667983@qq.com>
2022-07-28 22:48:26 +0800

    add final

---
+++ JavaBasis/src/main/java/com/qy/objectoriented/finalmodifier/finalclass/FinalClass.java
@@ -0,0 +1,9 @@
+package com.qy.objectoriented.finalmodifier.finalclass;
+
+/**
+ * @Author QianSiWang
+ * @Date 2022/7/28 19:53
+ * @Description
+ */
+public final class FinalClass {
+}
---
+++ JavaBasis/src/main/java/com/qy/objectoriented/finalmodifier/finalmethod/FinalFather.java
@@ -0,0 +1,17 @@
+package com.qy.objectoriented.finalmodifier.finalmethod;
+
+/**
+ * @Author QianSiWang
+ * @Date 2022/7/28 19:44
+ * @Description
+ */
+public class FinalFather {
+    public final void finalMethod(){
+        System.out.println("This is final method");
+    }
+
+    public void commonMethod(){
+        System.out.println("This is common method");
+    }
+
+}
