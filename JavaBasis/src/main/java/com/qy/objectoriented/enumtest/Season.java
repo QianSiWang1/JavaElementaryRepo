54922824f3d75c84085b303f6098a42e5ebe52a0
QianYu <597667983@qq.com>
2022-07-31 13:37:22 +0800

    add enum

---
+++ JavaBasis/src/main/java/com/qy/objectoriented/enumtest/Season.java
@@ -0,0 +1,28 @@
+package com.qy.objectoriented.enumtest;
+
+/**
+ * @Author QianSiWang
+ * @Date 2022/7/31 13:08
+ * @Description
+ */
+public enum Season {
+    SPRING("春天", 1),
+    SUMMER("夏天", 2),
+    FALL("秋天", 3),
+    WINTER("冬天", 4);
+
+    private final String name;
+    private final int quarter;
+
+    Season(String name, int quarter) {
+        this.name = name;
+        this.quarter = quarter;
+    }
+
+    public int getQuarter() {
+        return quarter;
+    }
+    public String getName() {
+        return name;
+    }
+}
