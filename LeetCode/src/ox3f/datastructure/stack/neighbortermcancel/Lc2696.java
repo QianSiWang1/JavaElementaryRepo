279d4752511759d9e784033daacf9a3518531533
59766 <qy19970630@163.com>
2025-05-05 21:09:31 +0800

    leetcode

---
+++ LeetCode/src/ox3f/datastructure/stack/neighbortermcancel/Lc2696.java
@@ -0,0 +1,11 @@
+package ox3f.datastructure.stack.neighbortermcancel;
+
+public class Lc2696 {
+    public int minLength(String s) {
+        while(s.contains("AB") || s.contains("CD")){
+            s = s.replaceAll("AB","");
+            s = s.replaceAll("CD","");
+        }
+        return s.length();
+    }
+}
--- LeetCode/src/ox3f/slidewindowandtwopointer/varslidewindow/Lc930.java
+++ LeetCode/src/ox3f/slidewindowandtwopointer/varslidewindow/Lc930.java
@@ -1,5 +1,21 @@
 package ox3f.slidewindowandtwopointer.varslidewindow;
 
-public class Lc930 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Lc930 {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-goal)){
+                res+=map.get(sum-goal);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return res;
+    }
 }
