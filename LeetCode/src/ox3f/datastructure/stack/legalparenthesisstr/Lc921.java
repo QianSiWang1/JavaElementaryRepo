c5891271a43fd236eddd0a64ffb92f06380e295d
59766 <qy19970630@163.com>
2025-05-07 23:35:21 +0800

    leetcode

---
+++ LeetCode/src/ox3f/datastructure/stack/legalparenthesisstr/Lc921.java
@@ -0,0 +1,21 @@
+package ox3f.datastructure.stack.legalparenthesisstr;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Lc921 {
+    public int minAddToMakeValid(String s) {
+        char[] charArray = s.toCharArray();
+        Deque<Character> deque = new ArrayDeque<>();
+        for(char c : charArray){
+            if(c == '('){
+                deque.push(c);
+            }
+            if(c == ')'){
+                if(!deque.isEmpty() && deque.peek() == '(');
+                deque.pop();
+            }
+        }
+        return deque.size();
+    }
+}
