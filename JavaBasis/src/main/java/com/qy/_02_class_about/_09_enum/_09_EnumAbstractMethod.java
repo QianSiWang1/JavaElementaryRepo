4ac310791f6d775cc86b78fe71e2cc65d64e7abd
qianyu <qy19970630@163.com>
2025-11-16 22:48:04 +0800

    枚举

---
+++ JavaBasis/src/main/java/com/qy/_02_class_about/_09_enum/_09_EnumAbstractMethod.java
@@ -0,0 +1,33 @@
+package com.qy._02_class_about._09_enum;
+
+/**
+ * 枚举可以添加抽象方法
+ */
+public enum _09_EnumAbstractMethod {
+    PLUS {
+        @Override
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS {
+        @Override
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES {
+        @Override
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE {
+        @Override
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    public abstract double apply(double x, double y);
+}
